1d888abffceb3f1d42829f25427c8e1f48769f1e
Al Momen <almomen.dev@gmail.com>
2026-02-19 13:49:32 +0600

    Add full Solvo project

---
+++ Solvo_P/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Solvo.Models;
+using Solvo_P.Models;
+using System.Collections.Generic;
+
+namespace Solvo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<SavingsGoal> SavingsGoals { get; set; }
+    }
+}
---
+++ Solvo_P/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Solvo_P.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
