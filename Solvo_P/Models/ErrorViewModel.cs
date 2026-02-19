1657649c00712f9d7874c152650a6d30a35e0527
Al Momen <almomen.dev@gmail.com>
2026-02-19 13:58:40 +0600

    Apply .gitignore and clean untracked files

--- Solvo_P/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Solvo_P.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
